6b635744d8726ed3192dfe718ce30a03a6628208
most025 <cristianmostazo2018@gmail.com>
2025-02-13 08:01:18 -0300

    Cambios de en la interfaz del programa

--- src/Gui.java
+++ src/Gui.java
@@ -1,6 +1,9 @@
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -11,11 +14,11 @@ import javax.swing.JTextField;
 
 public class Gui extends JFrame {
     public Gui() {
-        this.setSize(900, 900);
+        this.setSize(600, 400);
         this.setTitle("Sistema de entrada de cine");
-        this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setVisible(true);
         iniciarComponentes();
 
     }
@@ -25,45 +28,64 @@ public class Gui extends JFrame {
     }
 
     private void formulario() {
-        JPanel panelFormulario = new JPanel();
-        panelFormulario.setBackground(Color.GRAY);
+        JPanel panelFormulario = new JPanel(new GridBagLayout());
+        panelFormulario.setBackground(Color.LIGHT_GRAY);
         this.getContentPane().add(panelFormulario);
+
+        GridBagConstraints grid = new GridBagConstraints();
+        grid.insets = new Insets(10, 10, 10, 10); // este metodo añade un padding.
+        grid.fill = GridBagConstraints.HORIZONTAL;
+
 
         JLabel etiquetanombre = new JLabel("Nombre");
         etiquetanombre.setFont(new Font("Roboto", Font.PLAIN, 18));
         etiquetanombre.setForeground(Color.BLACK);
-        panelFormulario.add(etiquetanombre);
+        grid.gridx = 0;
+        grid.gridy = 0;
+        panelFormulario.add(etiquetanombre, grid);
 
-        JTextField campoNombre=new JTextField(20);
+        JTextField campoNombre = new JTextField(20);
         campoNombre.setFont(new Font("Roboto", Font.PLAIN, 20));
-        panelFormulario.add(campoNombre);
+        grid.gridx = 1;
+        grid.gridy = 0;
+        panelFormulario.add(campoNombre, grid);
 
-        JLabel etiquetaEdad=new JLabel("Edad");
-        etiquetaEdad.setFont(new Font("Roboto",Font.PLAIN, 18));
+        JLabel etiquetaEdad = new JLabel("Edad");
+        etiquetaEdad.setFont(new Font("Roboto", Font.PLAIN, 18));
         etiquetaEdad.setForeground(Color.BLACK);
-        panelFormulario.add(etiquetaEdad);
+        grid.gridx = 0;
+        grid.gridy = 1;
+        panelFormulario.add(etiquetaEdad, grid);
 
-        JTextField campoEdad= new JTextField(20);
+        JTextField campoEdad = new JTextField(20);
         campoEdad.setFont(new Font("Roboto", Font.PLAIN, 20));
-        panelFormulario.add(campoEdad);
+        grid.gridx = 1;
+        grid.gridy = 1;
+        panelFormulario.add(campoEdad, grid);
 
-        JLabel etiquetaDeOPciones=new JLabel("Selecionar Entradas");
+        JLabel etiquetaDeOPciones = new JLabel("Selecionar Entradas");
         etiquetaDeOPciones.setFont(new Font("Roboto", Font.PLAIN, 18));
         etiquetaDeOPciones.setForeground(Color.BLACK);
-        panelFormulario.add(etiquetaDeOPciones);
+        grid.gridx = 0;
+        grid.gridy = 2;
+        panelFormulario.add(etiquetaDeOPciones, grid);
 
-        String [] opciones={"Entrada General","Entrada VIP","Entrada para Estudiantes"};
+        String[] opciones = { "Entrada General", "Entrada VIP", "Entrada para Estudiantes" };
         @SuppressWarnings("rawtypes")
-        JComboBox  listadoDeOpciones= new JComboBox<>(opciones);
+        JComboBox listadoDeOpciones = new JComboBox<>(opciones);
         listadoDeOpciones.setFont(new Font("Roboto", Font.PLAIN, 18));
-        panelFormulario.add(listadoDeOpciones);
+        grid.gridx = 1;
+        grid.gridy = 2;
+        panelFormulario.add(listadoDeOpciones, grid);
 
-        JButton botonEnviar= new JButton();
+        JButton botonEnviar = new JButton();
         botonEnviar.setText("Enviar");
         botonEnviar.setFont(new Font("Roboto", Font.BOLD, 18));
         botonEnviar.setBackground(Color.BLUE);
         botonEnviar.setForeground(Color.WHITE);
-        panelFormulario.add(botonEnviar);
+        grid.gridx = 1;
+        grid.gridy = 3;
+        panelFormulario.add(botonEnviar, grid);
 
     }
 }
